6b2adddcc9a1aeb2f7c12e8f3fb6807b0fea6157
Steve Agalloco <steve.agalloco@gmail.com>
2013-04-05 15:05:36 -0400

    properly describe generator tasks

--- lib/generators/bower/install_generator.rb
+++ lib/generators/bower/install_generator.rb
@@ -5,7 +5,7 @@ module Bower
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../templates", __FILE__)
 
-      desc "Creates a Devise initializer and copy locale files to your application."
+      desc "Creates a local configuration file and the default JSON file for Bower to use in your application."
 
       def create_components_directory
         empty_directory 'components/javascripts'
